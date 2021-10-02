c98379880190df7166706486b8a9259bcc5ed1d2
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2021-10-02 23:30:00 +0530

    Video added

--- app/src/main/java/com/example/anapk/Surprise.java
+++ app/src/main/java/com/example/anapk/Surprise.java
@@ -1,20 +1,124 @@
 package com.example.anapk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class Surprise extends AppCompatActivity {
 TextView display;
+MediaPlayer mediaPlayer;
+SeekBar seekBar; Button play;
+Thread updateSeekBar;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.reset();
+        updateSeekBar.interrupt();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surprise);
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        seekBar = findViewById(R.id.seekBar);
+        play = findViewById(R.id.play);
         display = findViewById(R.id.display);
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
-        display.setText("Happy Birthday "+name);
+        display.setText("Happy Birthday "+name+"! \uD83E\uDD73\uD83C\uDF82\uD83C\uDF89\uD83C\uDF8A");
+        mediaPlayer = MediaPlayer.create(this,R.raw.vid);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        SurfaceHolder h = surfaceView.getHolder();
+
+        h.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder)
+            {
+                mediaPlayer.setDisplay(h);
+                surfaceView.setKeepScreenOn(true);
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.seekTo(0);
+                mediaPlayer.pause();
+              play.setText("Play");
+            }
+        });
+        updateSeekBar = new Thread(){
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+
+                while (currentPosition<totalDuration)
+                {
+                    try
+                    {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekBar.setProgress(currentPosition);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        seekBar.setMax(mediaPlayer.getDuration());
+        updateSeekBar.start();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                    play.setText("Play");
+                }
+                else {
+                    mediaPlayer.start();
+                    play.setText("Pause");
+                }
+            }
+        });
     }
 }
